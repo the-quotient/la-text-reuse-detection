705013f23357667ab8ee730cdcb4f0806260113e
thequotient <thequotient@jn2p.de>
2025-03-27 16:37:14 +0100

    predict function adapted to finetuned model

--- app/reranking.py
+++ app/reranking.py
@@ -1,31 +1,20 @@
+import numpy as np
 from sentence_transformers import CrossEncoder
 from tqdm import tqdm
 
 class Reranking:
-    
+
     def __init__(self, model_name, data):
         self.reranker = CrossEncoder(model_name)
         self.data = data
 
-    def rerank(self, threshold=0.5):
-        preds = self._compute_reranker_preds(threshold)
-        positive_pairs = []
-        for idx, pred in enumerate(preds):
-            if pred == 1:
-                pair = (self.data['sentence1'][idx],
-                        self.data['sentence2'][idx])
-                positive_pairs.append(pair)
-        return positive_pairs
-
-    def predict(self, threshold=0.5):
-        return self._compute_reranker_preds(threshold)
-
-    def _compute_reranker_preds(self, threshold):
-        reranker_preds = []
+    def predict(self):
+        preds = []
         sents1 = self.data['sentence1']
         sents2 = self.data['sentence2']
         for s1, s2 in tqdm(zip(sents1, sents2), total=len(sents1)):
-            score = self.reranker.predict([(s1, s2)])[0]
-            pred_label = int(score > threshold)
-            reranker_preds.append(pred_label)
-        return reranker_preds
+            logits = self.reranker.predict([(s1, s2)])
+            pred = int(np.argmax(logits, axis=1)[0])
+            preds.append(pred)
+        return preds
+
