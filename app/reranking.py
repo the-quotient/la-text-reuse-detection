1c9147e6e99e88f78767f3a4ef11709f3cd14f3b
thequotient <thequotient@jn2p.de>
2025-03-30 12:07:16 +0200

    App Interface as Notebook and Pipeline logic implemented

--- app/reranking.py
+++ app/reranking.py
@@ -1,20 +1,33 @@
 import numpy as np
+import pandas as pd
 from sentence_transformers import CrossEncoder
 from tqdm import tqdm
 
+
 class Reranking:
 
-    def __init__(self, model_name, data):
-        self.reranker = CrossEncoder(model_name)
+    def __init__(self, model_name, data=None):
+        self.reranker = CrossEncoder(
+            model_name,
+            max_length=256
+        )
         self.data = data
 
-    def predict(self):
-        preds = []
+    def get_logits(self):
         sents1 = self.data['sentence1']
         sents2 = self.data['sentence2']
+        result= []
         for s1, s2 in tqdm(zip(sents1, sents2), total=len(sents1)):
             logits = self.reranker.predict([(s1, s2)])
-            pred = int(np.argmax(logits, axis=1)[0])
-            preds.append(pred)
-        return preds
+            result.append(logits)
+        return result
 
+    def get_prediction(self, logits, threshold):
+        return (np.array(logits).flatten() > threshold).astype(int).tolist()
+
+    def predict(self, pairs, threshold):
+        df = pd.DataFrame(pairs, columns=["sentence1", "sentence2"])
+        self.data = df
+        logits = self.get_logits()
+        preds = self.get_prediction(logits, threshold)
+        return [pair for pair, pred in zip(pairs, preds) if pred == 1]
--- app/retrieval.py
+++ app/retrieval.py
@@ -3,8 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 class Retrieval:
-    
-    def __init__(self, model_name, queries, candidates):
+
+    def __init__(self, model_name, queries, candidates=None):
         print("Loading Sentence Transformer Model...")
         self.model = SentenceTransformer(model_name)
         self.query_sentences = queries
@@ -13,12 +13,23 @@ class Retrieval:
         self.query_embeddings = self.model.encode(
             queries, show_progress_bar=True
         )
-        print("\nEmbedding candidates...")
+        if candidates:
+            print("\nEmbedding candidates...")
+            self.candidate_embeddings = self.model.encode(
+                candidates, show_progress_bar=True
+            )
+        print("\n")
+
+
+    def embed_candidates(self, candidates):
+        self.candidate_sentences = candidates
+        print("Embedding candidates...")
         self.candidate_embeddings = self.model.encode(
             candidates, show_progress_bar=True
         )
-        print("\n")
-    
+        print(" Candidates embedded.\n")
+
+
     def retrieve(self, k=1, threshold=0.5):
         results = {}
         for query, q_emb in zip(self.query_sentences, self.query_embeddings):
@@ -32,3 +43,25 @@ class Retrieval:
             else:
                 results[query] = []
         return results
+
+
+    def retrieve_dual(self, k, fuzzy_threshold, retrieve_threshold):
+
+        fuzzy_quotes = []
+        to_rerank = []
+
+        for query, q_emb in zip(self.query_sentences, self.query_embeddings):
+            sims = cosine_similarity([q_emb], self.candidate_embeddings)[0]
+            sorted_indices = np.argsort(-sims)[:k]
+
+            for i in sorted_indices:
+                sim_score = sims[i]
+                candidate = self.candidate_sentences[i]
+
+                if sim_score >= fuzzy_threshold:
+                    fuzzy_quotes.append((query, candidate))
+                elif sim_score >= retrieve_threshold:
+                    to_rerank.append((query, candidate))
+
+        return fuzzy_quotes, to_rerank
+
